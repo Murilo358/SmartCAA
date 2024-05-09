2062015493a111d45b6ded8b41043efd2d0d1370
Murilo358 <murilobarbosa358@gmail.com>
2024-05-09 10:43:20 -0300

    created: search filter

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PaperSquare from "./PaperSquare";
 import {
   Box,
@@ -60,62 +60,79 @@ const projects = [
 ];
 
 const Project = () => {
+  const [searchFilter, setSearchFilter] = useState("");
+
+  const handleInputChange = (value) => {
+    console.log(value);
+    setSearchFilter(value);
+  };
+
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box>
-      <Box className="container mx-auto p-5 mt-5 flex flex-col items-center  ">
-        <Box className="flex text-center">
-          <Header
-            titleVariant="h1"
-            subTitleVariant="h3"
-            title="Projetos"
-            subtitle="Visualize seus projetos"
-          />
-        </Box>{" "}
-        <Box
-          sx={{
-            border: `2px solid ${colors.darkGreen[300]}`,
+    <Box className="container mx-auto p-5 mt-5 flex flex-col items-center ">
+      <Box className="flex text-center">
+        <Header
+          titleVariant="h1"
+          subTitleVariant="h3"
+          title="Projetos"
+          subtitle="Visualize seus projetos"
+        />
+      </Box>{" "}
+      <Box
+        sx={{
+          border: `2px solid ${colors.darkGreen[300]}`,
+        }}
+        className="flex mb-10 w-[80%]  rounded-full"
+      >
+        <TextField
+          InputProps={{
+            disableUnderline: true,
           }}
-          className="flex mb-10 w-[80%]  rounded-full"
-        >
-          <TextField
-            InputProps={{
-              disableUnderline: true,
-            }}
-            sx={{
-              fontSize: 50,
-              "& .MuiInput-root": {
-                fontWeight: "bold",
-                fontSize: "22px",
-                marginLeft: "10px",
-                color: colors.primary[100],
-                border: "none",
-                padding: "2px",
-              },
-            }}
-            className="w-full !border-none"
-            variant="standard"
-            placeholder="Pesquisar..."
-          />
-          <Button
-            className="rounded-full"
-            sx={{
-              backgroundColor: colors.greenAccent[400],
-              borderRadius: 1000,
-            }}
-            variant="oulined"
-            startIcon={<SearchIcon />}
-          />
-        </Box>
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 3, sm: 8, md: 4, lg: 8, xl: 12 }}
-        >
-          {projects.length > 0
-            ? projects.map((item) => (
+          onChange={(e) => handleInputChange(e.target.value)}
+          sx={{
+            fontSize: 50,
+            "& .MuiInput-root": {
+              fontWeight: "bold",
+              fontSize: "22px",
+              marginLeft: "10px",
+              color: colors.primary[100],
+              border: "none",
+              padding: "2px",
+            },
+          }}
+          className="w-full !border-none"
+          variant="standard"
+          placeholder="Pesquisar..."
+        />
+        <Button
+          className="rounded-full"
+          sx={{
+            backgroundColor: colors.greenAccent[400],
+            borderRadius: 1000,
+          }}
+          variant="oulined"
+          startIcon={<SearchIcon />}
+        />
+      </Box>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 3, sm: 8, md: 4, lg: 8, xl: 12 }}
+      >
+        {projects.length > 0
+          ? projects
+              .filter(
+                (item) =>
+                  item.title
+                    .toLowerCase()
+                    .includes(searchFilter.toLowerCase()) ||
+                  item.subtitle
+                    .toLowerCase()
+                    .includes(searchFilter.toLowerCase())
+              )
+              .map((item) => (
                 <Grid key={item} item xs={2} sm={4} lg={3} md={2} xl={3}>
                   {" "}
                   <Link to={item.link}>
@@ -123,9 +140,8 @@ const Project = () => {
                   </Link>
                 </Grid>
               ))
-            : ""}
-        </Grid>
-      </Box>
+          : ""}
+      </Grid>
     </Box>
   );
 };
